2401aeab1b6ea4fc15b99bab7fa0f595fa68c73b
Nadeesha Fernando <nadeeshafernando2020@gmail.com>
2024-11-22 16:31:58 +0530

    Temporary Deleted.

--- $/cat.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if len(sys.argv) < 2:
-    print("Usage: cat.py filename")
-else:
-    filename = sys.argv[1]
-    with open(filename, 'r') as file:
-        print(file.read())
--- $/edit-path.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.run(["C:\\Windows\\system32\\rundll32.exe", "sysdm.cpl,EditEnvironmentVariables"])
